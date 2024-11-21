57d77649cdfa743cf85656687b3a3361b5a2b36f
Riwane Aberkane <riwane.aberkane.pro@outlook.fr>
2024-11-21 17:04:30 +0100

    wip...

--- scripts.js
+++ scripts.js
@@ -2,19 +2,14 @@ window.addEventListener('load', () => {
     const typingDemo = document.querySelector('.typing-demo');
     const logo = document.getElementById('logo');
 
-    // Lorsque l'animation de la phrase est terminée, on affiche le logo
+    // Lorsque l'animation de texte est terminée
     typingDemo.addEventListener('animationend', () => {
-        logo.style.opacity = '2'; // Rendre le logo visible
-        logo.style.transform = 'translateY(0)'; // Le faire glisser vers le bas
+        setTimeout(() => {
+            logo.classList.add('show-logo');
+        }, 500);
+
+        setTimeout(() => {
+            document.querySelector('.socials').classList.add('show-socials');
+        }, 1400);
     });
 });
-
-window.onload = function () {
-    // Ajoutez la classe show-logo au logo pour déclencher l'animation
-    document.getElementById('logo').classList.add('show-logo');
-
-    // Après 1.5 secondes (pour permettre au logo de finir son animation), affichez les réseaux sociaux
-    setTimeout(function () {
-        document.querySelector('.socials').classList.add('show-socials');
-    }, 800); // Temps ajusté pour correspondre à la durée de l'animation du logo
-};
