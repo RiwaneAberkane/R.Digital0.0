4d39e85432067e4c4e001c588cb06190ee83e16c
Riwane Aberkane <riwane.aberkane.pro@outlook.fr>
2024-12-02 01:58:47 +0100

    wip...

--- scripts.js
+++ scripts.js
@@ -1,15 +1,23 @@
 window.addEventListener('load', () => {
     const typingDemo = document.querySelector('.typing-demo');
     const logo = document.getElementById('logo');
+    const socials = document.querySelector('.socials');
+    let animationPlayed = false; // Empêche la relance de l'animation
 
-    // Lorsque l'animation de texte est terminée
+    // Attendre la fin de l'animation de texte
     typingDemo.addEventListener('animationend', () => {
-        setTimeout(() => {
-            logo.classList.add('show-logo');
-        }, 500);
+        if (!animationPlayed) {
+            animationPlayed = true; // Marquer comme joué
 
-        setTimeout(() => {
-            document.querySelector('.socials').classList.add('show-socials');
-        }, 1400);
+            // Afficher le logo avec un délai de 0.5 seconde
+            setTimeout(() => {
+                logo.classList.add('show-logo');
+            }, 500);
+
+            // Afficher les réseaux sociaux après le logo
+            setTimeout(() => {
+                socials.classList.add('show-socials');
+            }, 2000);
+        }
     });
 });
